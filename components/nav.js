2cf879b1a8698a0ec7f1c9c74427f1c4fa71d4d2
Achuth <armorachyuth123@gmail.com>
2020-07-15 05:56:35 +0000

    initital commit

--- components/nav.js
+++ components/nav.js
@@ -1,164 +1,35 @@
-import React, { useState } from 'react'
-import Link from 'next/link'
-import Router from 'next/router'
-import styled from 'styled-components'
+import React, { useState } from 'react' 
 import Icon from 'react-icons-kit'
-import { x, moon } from 'react-icons-kit/feather'
-const Nav = (props) => {
-    const [menu, setMenu] = useState(false)
-    return (
-        <NavWrapper>
-            <Logo />
-            <ul className="desk-nav">
-                <li>
-                    <Link href="/about">
-                        <a>UI</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/about">
-                        <a>UX</a>
-                    </Link></li>
-                <li>
-                    <Link href="/about">
-                        <a>DX</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/about">
-                        <a>PROJECTS</a>
-                    </Link>
-                </li>
-                <li onClick={() => { props.OnThemeChange() }}>
-                    <span><Icon icon={moon} /></span>
-                </li>
-            </ul>
-            <MenuIcon onClick={() => { setMenu(!menu) }} />
-            {menu && <MenuSheet>
-                <span onClick={() => { setMenu(!menu) }}><Icon icon={x} style={{ cursor: 'pointer' }} /></span>
-                <ul>
-                    <li>
-                        <Link href="/about">
-                            <a>UI</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/about">
-                            <a>UX</a>
-                        </Link></li>
-                    <li>
-                        <Link href="/about">
-                            <a>DX</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/about">
-                            <a>PROJECTS</a>
-                        </Link>
-                    </li>
-                    <li onClick={() => { props.OnThemeChange() }}>
-                        <i><Icon icon={moon} /></i>
-                    </li>
-                </ul>
-            </MenuSheet>}
-        </NavWrapper>
+import NavLink from 'next/link'
+import { moon, sun } from 'react-icons-kit/feather'
+import { theme } from '../layouts/page'
+import styled from 'styled-components'
+
+
+const Nav = (props) => { 
+    return ( 
+        <nav style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', alignContent: 'center', padding: '20px 0px' }}>
+            <h3 style={{ fontFamily: 'monospace', flex: 1 }}>  -///-   </h3>
+            <NavWrapper>
+                <NavLink href="/"><a style={{ padding: '5px 10px' }}>Home</a></NavLink>
+                {/* <NavLink href="/projects"><a style={{ padding: '5px 10px' }}>Projects</a></NavLink> */}
+                <NavLink href="/blog"><a style={{ padding: '5px 10px' }}>Blog</a></NavLink>
+                <NavLink href="/now"><a style={{ padding: '5px 10px' }}>Now</a></NavLink>
+               <span style={{ padding: '5px 10px' }} onClick={() => { props.changeTheme() }}><Icon icon={props.theme === theme.light ? moon : sun} /></span>
+            </NavWrapper>
+           
+        </nav>
+             
     )
 }
 
-export default Nav
-const Logo = () => <LogoWrapper>
-                        <Link href="/">
-                            <a>
-                                UIUXDX
-                        </a>
-                        </Link>
-                    </LogoWrapper>;
-const LogoWrapper = styled.h3`
-    flex:1;
-    font-size:24px;
-    font-family: monospace;
-    font-weight:900; 
-    padding:10px;
-    @media (max-width: 600px) {
-        margin:0;
-    }
-    `
+export default Nav ;
 
-const NavWrapper = styled.nav`
-    display:flex;
-    flex:1; 
-    margin:auto;
-    padding:2% 5%;
-    .desk-nav{
-        display:flex;
-        list-style:none;
-        flex:0.5;
-        font-size:.7em;
-        @media (max-width: 600px) {
-          display:none;
-      }
-      li{
-          padding:10px;
-          flex:1; 
-      }
-    }
-`;
-
-const MenuIcon = styled.span`
-cursor:pointer;
-@media (min-width: 600px) {
-    display:none;
-}
-&::before{
-    content:'';
-    margin:5px; 
-    display:block;
-    height:3px;
-    background:${props => props.theme.color};
-    width:20px;
-    border-radius:5px;
-}
-&::after{
-    content:'';
-    margin:5px; 
-    display:block;
-    height:3px;
-    background:${props => props.theme.color};
-    width:10px;
-    border-radius:5px;
-}
-`
-
-const MenuSheet = styled.div` 
+const NavWrapper = styled.div`
     display:flex; 
-    align-items:center;
-    text-align:center;
-    justify-content:center;
-    position:fixed;
-    z-index:9999;
-    background:${props => props.theme.primary};
-    top:0;
-    left:0;
-    color:${props => props.theme.color};
-    height:100%;
-    width:100%;
-    transition:all 1s ease-in-out;
-    @media (min-width: 600px) {
-            display:none;
-        }
-    span{
-       position:absolute;
-       top:10%;
-       right:10%; 
-    }
-    ul{
-        list-style:none;
-        flex:1;
-        margin:0;
-        padding:0px 20px;
-        li{
-            padding:10px 0px; 
-            flex:1;
-        }
+    list-style:none;   
+    a:hover{
+        background:${props => props.theme.tint}; 
+        border-radius:5px;
     }
 `
--- layouts/page.js
+++ layouts/page.js
@@ -1,67 +1,68 @@
 import { Component } from 'react'
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components'
 import Router from 'next/router'
 import Progress from 'nprogress'
 import Nav from '../components/nav';
 // Global Styles
 const GlobalStyle = createGlobalStyle`
- 
-html,body{
-  margin:0;
-  padding:0;  
-  position:relative;
-  height:100%;
-  width:100%;
-  font-size:24px;
-  background:${props=>props.theme.primary};
-  color:${props=>props.theme.color};
-  transition:.3s ease-in-out;
-}
+    html,body{
+    margin:0;
+    padding:0;  
+    position:relative;
+    height:100%;
+    width:100%;
+    font-size:24px;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                sans-serif;
+    background:${props => props.theme.background};
+    color:${props => props.theme.color};
+    transition:.3s ease-in-out;
+    }  
+    *,:after,:before {
+        padding: 0;
+        margin: 0;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+        -webkit-box-shadow: none;
+        box-shadow: none;
+        outline: none
+    }
 
-*,:after,:before {
-    padding: 0;
-    margin: 0;
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-    -webkit-box-shadow: none;
-    box-shadow: none;
-    outline: none
-}
+    ::-webkit-scrollbar {
+        width: 14px;
+        height: 14px
+    }
 
-::-webkit-scrollbar {
-    width: 14px;
-    height: 14px
-}
+    ::-webkit-scrollbar-track {
+        background-color: transparent
+    }
 
-::-webkit-scrollbar-track {
-    background-color: transparent
-}
+    ::-webkit-scrollbar-thumb,::-webkit-scrollbar-track {
+        border: 4px solid transparent;
+        background-clip: padding-box;
+        border-radius: 8px
+    }
 
-::-webkit-scrollbar-thumb,::-webkit-scrollbar-track {
-    border: 4px solid transparent;
-    background-clip: padding-box;
-    border-radius: 8px
-}
+    ::-webkit-scrollbar-thumb {
+        background-color: ${props => props.theme.color}
+    }
 
-::-webkit-scrollbar-thumb {
-    background-color: ${props=>props.theme.color}
-}
+    ::-webkit-scrollbar-button {
+        display: none;
+        width: 0;
+        height: 0
+    }
 
-::-webkit-scrollbar-button {
-    display: none;
-    width: 0;
-    height: 0
-}
-
-::-webkit-scrollbar-corner {
-    background-color: transparent
-}
-#__next{
-  padding:0;
-  margin:0;   
-  font-family:sans-serif;
-}
-input[type="submit"] {
+    ::-webkit-scrollbar-corner {
+        background-color: transparent
+    }
+    #__next{
+    padding:0;
+    margin:0;   
+    font-family:sans-serif;
+    }
+    input[type="submit"] {
       padding: 5px 15px;
       background: #ccc;
       border: 0 none;
@@ -109,8 +110,102 @@ input[type="submit"] {
         box-shadow: 0 0 10px #5d9e6b, 0 0 5px #5d9e6b;
         opacity: 1;
         transform: rotate(3deg) translate(0px, -4px);
-      }
-`; 
+      } 
+        main {
+          padding: 5rem 0;
+          flex: 1; 
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+        .title a {
+          color: palegreen;
+          text-decoration: none;
+        }
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 2rem;
+        }
+ 
+        .description {
+          line-height: 1.5;
+          font-size: 1.2rem;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: palegreen;
+          border-color: palegreen;
+        }
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+        .logo {
+          height: 1em;
+        }
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+`;
 let progress
 const stopProgress = () => {
     clearTimeout(progress)
@@ -124,25 +219,26 @@ Router.onRouteChangeStart = () => {
 Router.onRouteChangeComplete = stopProgress
 Router.onRouteChangeError = stopProgress
 
-const light = {
-    color: '#000',
-    primary: '#fff',
-    secondary: '#000',
-    accent: '#5d9e6b'
-};
-const dark = {
-    color: '#fff',
-    primary: '#000',
-    secondary: '#181a1d',
-    accent: "#5d9e6b"
+export const theme = {
+    light: {
+        background: '#fefefe',
+        color: '#121212',
+        tint:'rgba(0,0,0,.1)',
+        accent:'palegreen'
+    },
+    dark: {
+        background: '#001321',
+        color: '#fefefe',
+        tint:'rgba(4,4,4,.7)',
+        accent:'palegreen'
+    }
 }
-
 class Page extends Component {
     constructor() {
         super();
-        this.state = { 
-            theme: light
-        } 
+        this.state = {
+            theme: theme.dark
+        }
         this.handleKeypress = this.handleKeypress.bind(this)
     }
     componentDidMount() {
@@ -152,37 +248,45 @@ class Page extends Component {
         document.removeEventListener('keydown', this.handleKeypress, true)
     }
 
-    handleKeypress(event) {  
-    if (event.altKey  && event.keyCode === 78 ) { 
-      return Router.push({
-        pathname: '/new'
-      })
+    handleKeypress(event) {
+        if (event.altKey && event.keyCode === 78) {
+            return Router.push({
+                pathname: '/new'
+            })
+        }
+        if (event.altKey && event.keyCode === 83) {
+            return Router.push({
+                pathname: '/settings'
+            })
+        }
+
+        if (event.altKey && event.keyCode === 37) {
+            return Router.push({
+                pathname: '/home'
+            })
+        }
     }
-    if (event.altKey && event.keyCode === 83 ) { 
-      return Router.push({
-        pathname: '/settings'
-      })
-    }
-    
-    if (event.altKey && event.keyCode === 37 ) { 
-      return Router.push({
-        pathname: '/home'
-      })
-    }
-}
 
 
     render() {
         const { children } = this.props;
         return (
             <>
-               <ThemeProvider theme={this.state.theme}>
+                <ThemeProvider theme={this.state.theme}>
                     <GlobalStyle />
-                     <Nav OnThemeChange = {()=>{this.setState({theme: this.state.theme === light ? dark : light})}}/>
-                    {children}
+                    <Wrapper>
+                        <Nav theme={this.state.theme} changeTheme={() => { this.setState({ theme: this.state.theme === theme.light ? theme.dark : theme.light }) }} />
+                        {children}
+                    </Wrapper>
                 </ThemeProvider>
             </>
         )
     }
 }
-export default Page
+export default Page;
+
+const Wrapper = styled.div`
+    max-width:800px;
+    margin:auto;
+    font-size:18px;
+`
