cd3270cb7f2cebab56c07ebf00f1d0c81fc88852
Achuth <armorachyuth123@gmail.com>
2020-11-01 14:17:11 +0000

    resolved few issues

--- components/Nav.js
+++ components/Nav.js
@@ -3,40 +3,26 @@ import NavLink from 'next/link';
 import Link from './Link';
 export default () => {
   return (
-    <DeskTopNav>
-      <Header>
-        <h3 style={{ fontFamily: 'monospace', flex: 1, fontWeight: 700 }}>
-          <NavLink href="/">
-            <a>-///-</a>
-          </NavLink>
-        </h3>
-        <span style={{ flex: 1 }} />
-        <NavWrapper>
-          <Link to="/" label="home" />
-          <Link to="/blog" label="articles" />
-          <Link to="/#projects" label="Projects" />
-          <Link to="/#contact" label="Contact" />
-          {/* <NavLink href="/now">
+    <Header>
+      <h3 style={{ fontFamily: 'monospace', flex: 1, fontWeight: 700 }}>
+        <NavLink href="/">
+          <a>-///-</a>
+        </NavLink>
+      </h3>
+      <span style={{ flex: 1 }} />
+      <NavWrapper>
+        <Link to="/" label="home" />
+        <Link to="/blog" label="articles" />
+        <Link to="/#projects" label="Projects" />
+        <Link to="/#contact" label="Contact" />
+        {/* <NavLink href="/now">
                             <a style={{ padding: '10px 15px' }}>Now</a>
                         </NavLink> */}
-        </NavWrapper>
-        {/* <ThemedButton /> */}
-      </Header>
-    </DeskTopNav>
+      </NavWrapper>
+      {/* <ThemedButton /> */}
+    </Header>
   );
 };
-
-const DeskTopNav = styled.div`
-  display: flex;
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  align-content: center;
-  padding: 20px 0px;
-  text-transform: uppercase;
-  opacity: 0;
-  animation: bcCCNc 0.6s 0.3s ease-in-out forwards;
-`;
 
 const NavWrapper = styled.div`
   display: flex;
@@ -46,7 +32,6 @@ const NavWrapper = styled.div`
 const Header = styled.header`
   position: sticky;
   top: 0;
-  padding: 5px;
   flex: 1;
   max-width: 1200px;
   display: flex;
@@ -55,6 +40,10 @@ const Header = styled.header`
   backdrop-filter: saturate(180%) blur(20px);
   display: flex;
   align-items: center;
+  margin: auto;
+  padding: 20px 0px;
+  text-transform: uppercase;
+  animation: bcCCNc 0.6s 0.3s ease-in-out forwards;
   @media (max-width: 960px) {
     & {
       flex-direction: column;
