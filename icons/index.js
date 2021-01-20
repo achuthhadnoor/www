6ec5400f95a3320cb2bf60e3837b0ea800ed3eda
Achuth Hadnoor <achuth_hadnoor@epam.com>
2021-01-20 20:08:43 +0530

    changed to typescript

--- icons/index.js
+++
@@ -1,9 +0,0 @@
-import { Linkedin } from './Linkedin';
-import { Mail } from './Mail';
-import { Dribbble } from './Dribbble';
-import { Instagram } from './Instagram';
-import { Github } from './Github';
-import { Twitter } from './Twitter';
-import { Share } from './Share';
-
-export { Github, Instagram, Dribbble, Mail, Linkedin, Twitter, Share };
