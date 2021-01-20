ab1a23cc349c9a7bf6e66cccfe40514240ec461f
Achuth Hadnoor <armorachyuth123@gmail.com>
2021-01-20 21:32:25 +0530

    footer change

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Social from '../components/social';
 
@@ -5,6 +6,7 @@ const Home = () => (
   <div>
     <Header />
     <Social />
+    <Footer />
   </div>
 );
 
