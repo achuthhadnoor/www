b3910f45a42d209d5b8e3b289ddfda3d63699f80
Achuth Hadnoor <armorachyuth123@gmail.com>
2021-04-25 15:10:36 +0530

    seo to the page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
+import { NextSeo } from 'next-seo';
 import Header from '../components/header'
 import Footer from '../components/footer'
 import Social from '../components/social'
 import Subscribe from '../components/subscribe'
 import Articles from '../components/Home/recentArticles'
+import SEO from '../next-seo.config';
 
 function Home() {
   return (
@@ -12,6 +14,11 @@ function Home() {
     <title>Achuth Hadnoor – Developer, writer, maker.</title>
     <meta name="description" content="Developer, JavaScript enthusiast, and product maker."/>
  </Head>
+ <NextSeo
+      title="Simple Usage Example"
+      description="A short description goes here."
+      {...SEO}
+    />
     <div className="container max-w-5xl mx-auto">
       <Header />
       <main className="mx-auto mt-5 px-4">
