f3058bb7c384a578185ee9c2d42fb57cab5ed299
Achuth Hadnoor <armorachyuth123@gmail.com>
2021-04-26 18:30:51 +0530

    small fixes

---
+++ components/Home/blogpostsList.js
@@ -0,0 +1,7 @@
+import {frontMatter as alffiliate} from '../../pages/blog/affiliate-marketing.mdx'
+import {frontMatter as branding} from '../../pages/blog/personal-branding-over-portfolios.mdx' 
+
+export default [
+    alffiliate,
+    branding,
+]
--- components/Home/recentArticles.js
+++ components/Home/recentArticles.js
@@ -1,56 +1,36 @@
 import Link from "next/link"
+import React from "react";
+
+import list from './blogpostsList'
 
 const Articles = () => {
-    const articles = [
-        {
-            title: "Personal branding is more influential than long list of portfolio projects",
-            summary: "Portfolio projects help you undertand the design problems in the existing design project. but personal branding helps you create a design system",
-            tags: [{
-                id: "design",
-                value: "design",
-                style: "text-indigo-400 bg-indigo-50"
-            }, {
-                id: "personal",
-                value: "personal",
-                style: "text-pink-400 bg-pink-50"
-            }],
-            link: "/blog/personal-branding-over-portfolios"
-        },
-        {
-            title: "What i learned from a $1k through affiliate marketing",
-            summary: "Lessons and takeaways from my experience of bringing apps to uses using affiliate marketers",
-            tags: [{
-                id: "marketing",
-                value: "marketing",
-                style: "bg-red-50  text-red-400"
-            }],
-            link: "/blog/affiliate-marketing"
-        }
-    ]
     return (
         <>
             <h1 className="text-xl mt-5 font-semibold">Recent Articles</h1>
             <div className="grid mb-10 sm:grid-cols-2 sm:mx-0 ">
                 {
-                    articles.map((article, i) => (
-                        <Link href={article.link} key={`article-${i}`}>
-                            <a>
-                                <div className="mt-5 max-w-md">
-                                    <div className="my-2 text-xs">
-                                        <span className="text-gray-500">April 24,2021</span>
-                                        {
-                                            article.tags.map((tag,index) => (
-                                                <span className={`px-1 py-1 mx-1 rounded-md ${tag.style}`} key={`tag-${index}`}> {tag.value}</span>
-                                            )
-                                            )}
+                    list.map((article, i) => (
+                        <React.Fragment key={`article-${i}`}>
+                            {i < 4 && <Link href={article.slug}>
+                                <a>
+                                    <div className="mt-5 max-w-md">
+                                        <div className="my-2 text-xs">
+                                            <span className="text-gray-500">April 24,2021</span>
+                                            {
+                                                article.tags.map((tag, index) => (
+                                                    <span className={`px-1 py-1 mx-1 rounded-md ${tag.style}`} key={`tag-${index}`}> {tag.value}</span>
+                                                )
+                                                )}
+                                        </div>
+                                        <div>
+                                            <h3 className="font-semibold">{article.title}</h3>
+                                            <p className="text-gray-600">{article.summary}</p>
+                                        </div>
                                     </div>
-                                    <div>
-                                        <h3 className="font-semibold">{article.title}</h3>
-                                        <p className="text-gray-600">{article.summary}</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </Link>
+                                </a>
+                            </Link>
+                            }
+                        </React.Fragment>
                     ))
                 }
             </div>
