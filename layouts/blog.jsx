7f81297b802032d75b5c7d5b163a9c2234b57182
Achuth Hadnoor <armorachyuth123@gmail.com>
2021-04-28 20:15:41 +0530

    mdx components

--- layouts/blog.jsx
+++ layouts/blog.jsx
@@ -15,74 +15,76 @@ const discussUrl = (slug) =>
 export default function BlogLayout({ children, frontMatter }) {
   return (
     <Container
-      title={`${frontMatter.title} – Achuth Hadnoor`}
-      description={frontMatter.summary}
-      image={`https://achuth.dev${frontMatter.ogimage}`}
-      date={new Date(frontMatter.publishedAt).toISOString()}
-      type="article"
-    >
-      <div className="flex text-sm text-gray-200 mt-10 px-4 sm:mx-0">
+    title={`${frontMatter.title} – Achuth Hadnoor`}
+    description={frontMatter.summary}
+    image={`https://achuth.dev${frontMatter.ogimage}`}
+    date={new Date(frontMatter.publishedAt).toISOString()}
+    type="article"
+>
+    <div className="flex text-sm text-gray-200 mt-10 px-4 sm:mx-0">
         <Link href="/blog"><a className="uppercase">← Blog</a></Link><span className="flex-1"></span><div className="uppercase"><span className="px-2 py-1">←</span> posts <span className="px-2 py-1">→</span></div>
-      </div>
-      <article className="mx-auto max-w-mb mt-5 px-4">
+    </div>
+    <article className="mx-auto max-w-mb mt-5 px-4">
         <h1 className="py-2 font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white ">
-          {frontMatter.title}
+            {frontMatter.title}
         </h1>
-        <div className="flex flex-col py-1 md:flex-row justify-between items-start md:items-center w-full mt-2 mb-8 sm:flex-row">
-          <div className="flex items-center">
-            <Image
-              alt="Achuth Hadnoor"
-              height={24}
-              width={24}
-              src="/images/avatar.png"
-              className="rounded-full"
-            />
-            <p className="text-sm text-gray-700 dark:text-gray-300 ml-2">
-              {frontMatter.by}
-              {'Achuth Hadnoor / '}
-              {format(parseISO(frontMatter.publishedAt), 'MMMM dd, yyyy')}
+        <div className="">
+            <div className="flex items-center">
+                <Image
+                    alt="Achuth Hadnoor"
+                    height={24}
+                    width={24}
+                    src="/images/avatar.png"
+                    className="rounded-full"
+                />
+                <p className="text-sm text-gray-700 dark:text-gray-300 ml-2">
+                    {frontMatter.by}
+                    {'Achuth Hadnoor / '}
+                    {format(parseISO(frontMatter.publishedAt), 'MMMM dd, yyyy')}
+                </p>
+            </div>
+            <p className="inline-block pl-8 pr-2 text-sm text-gray-500 min-w-32 mt-2 md:mt-0">
+                {frontMatter.readingTime.text}
+                {` • `} 20 views
+                {/* <ViewCounter slug={frontMatter.slug} /> */}
             </p>
-          </div>
-          <p className="inline-block pl-10 pr-2 text-sm text-gray-500 min-w-32 mt-2 md:mt-0">
-            {frontMatter.readingTime.text}
-            {` • `}
-            {/* <ViewCounter slug={frontMatter.slug} /> */}
-          </p>
         </div>
         <div className="my-2">{
-          frontMatter.tags.map((tag, index) => (
-            <span className={`uppercase px-2 py-1 mx-2 rounded-md bg-gray-200 text-gray-600 text-xs`} key={`tag-${index}`}> {tag.value}</span>
-          )
-          )}</div>
-          <Image
-              alt="Achuth Hadnoor" 
-              src={frontMatter.image}
-              width={1175} height={600} priority
-            />
+            frontMatter.tags.map((tag, index) => (
+                <span className={`uppercase px-2 py-1 mx-2 rounded-md bg-gray-200 text-gray-600 text-xs`} key={`tag-${index}`}> {tag.value}</span>
+            )
+            )}
+        </div>
+        <Image
+            alt="Achuth Hadnoor"
+            src={frontMatter.image}
+            width={1175} height={600} priority
+            className="mt-5"
+        />
         <div className="prose dark:prose-dark max-w-none w-full mt-5">
-          {children}
+            {children}
+        </div>
+        <div className="text-sm text-gray-700 dark:text-gray-300 text-center">
+            <a
+                href={discussUrl(frontMatter.slug)}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                {'Discuss on Twitter'}
+            </a>
+            {` • `}
+            <a
+                href={editUrl(frontMatter.slug)}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                {'Edit on GitHub'}
+            </a>
         </div>
         <div className="mt-8">
-          <Subscribe />
+            <Subscribe />
         </div>
-        <div className="text-sm text-gray-700 dark:text-gray-300">
-          <a
-            href={discussUrl(frontMatter.slug)}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {'Discuss on Twitter'}
-          </a>
-          {` • `}
-          <a
-            href={editUrl(frontMatter.slug)}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {'Edit on GitHub'}
-          </a>
-        </div>
-      </article>
-    </Container>
-  );
+    </article>
+</Container>
+);
 }
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,92 +1,17 @@
-import Image from 'next/image';
-import Link from 'next/link'
-import { parseISO, format } from 'date-fns';
-import Container from '../../components/Container';
-import Subscribe from '../../components/Subscribe';
 import { getFileBySlug, getFiles } from "../../lib/mdx";
-
-
-const editUrl = (slug) =>
-    `https://github.com/achuthhadnoor/www/edit/master/data/blog/${slug}.mdx`;
-const discussUrl = (slug) =>
-    `https://mobile.twitter.com/search?q=${encodeURIComponent(
-        `https://achuth.dev/blog/${slug}`
-    )}`;
-
+import hydrate from 'next-mdx-remote/hydrate';
+import BlogPost from '@/layouts/blog';
+import MDXComponents from '@/components/MDXComponents'
 
 export default function Post({ mdxSource, frontMatter }) {
-
+    const content = hydrate(mdxSource, {
+        components: {
+              ...MDXComponents,
+            //   StaticTweet
+        }
+    }); 
     return (
-        <Container
-            title={`${frontMatter.title} – Achuth Hadnoor`}
-            description={frontMatter.summary}
-            image={`https://achuth.dev${frontMatter.ogimage}`}
-            date={new Date(frontMatter.publishedAt).toISOString()}
-            type="article"
-        >
-            <div className="flex text-sm text-gray-200 mt-10 px-4 sm:mx-0">
-                <Link href="/blog"><a className="uppercase">← Blog</a></Link><span className="flex-1"></span><div className="uppercase"><span className="px-2 py-1">←</span> posts <span className="px-2 py-1">→</span></div>
-            </div>
-            <article className="mx-auto max-w-mb mt-5 px-4">
-                <h1 className="py-2 font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white ">
-                    {frontMatter.title}
-                </h1>
-                <div className="flex flex-col py-1 md:flex-row justify-between items-start md:items-center w-full mt-2 mb-8 sm:flex-row">
-                    <div className="flex items-center">
-                        <Image
-                            alt="Achuth Hadnoor"
-                            height={24}
-                            width={24}
-                            src="/images/avatar.png"
-                            className="rounded-full"
-                        />
-                        <p className="text-sm text-gray-700 dark:text-gray-300 ml-2">
-                            {frontMatter.by}
-                            {'Achuth Hadnoor / '}
-                            {format(parseISO(frontMatter.publishedAt), 'MMMM dd, yyyy')}
-                        </p>
-                    </div>
-                    <p className="inline-block pl-10 pr-2 text-sm text-gray-500 min-w-32 mt-2 md:mt-0">
-                        {frontMatter.readingTime.text}
-                        {` • `}
-                        {/* <ViewCounter slug={frontMatter.slug} /> */}
-                    </p>
-                </div>
-                <div className="my-2">{
-                    frontMatter.tags.map((tag, index) => (
-                        <span className={`uppercase px-2 py-1 mx-2 rounded-md bg-gray-200 text-gray-600 text-xs`} key={`tag-${index}`}> {tag.value}</span>
-                    )
-                    )}</div>
-                <Image
-                    alt="Achuth Hadnoor"
-                    src={frontMatter.image}
-                    width={1175} height={600} priority
-                />
-                <div className="prose dark:prose-dark max-w-none w-full mt-5">
-                    {/* {children} */}
-                </div>
-                <div className="mt-8">
-                    <Subscribe />
-                </div>
-                <div className="text-sm text-gray-700 dark:text-gray-300">
-                    <a
-                        href={discussUrl(frontMatter.slug)}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        {'Discuss on Twitter'}
-                    </a>
-                    {` • `}
-                    <a
-                        href={editUrl(frontMatter.slug)}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        {'Edit on GitHub'}
-                    </a>
-                </div>
-            </article>
-        </Container>
+        <BlogPost frontMatter={frontMatter}>{content}</BlogPost>
     )
 }
 
