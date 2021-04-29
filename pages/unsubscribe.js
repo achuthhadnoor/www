877412e34f9f05e78e89dfe80e182ca87fcdddd0
Achuth Hadnoor <armorachyuth123@gmail.com>
2021-04-29 12:06:35 +0530

    unsubscribe

---
+++ pages/unsubscribe.js
@@ -0,0 +1,10 @@
+import Container from "@/components/Container";
+
+const Unsubscribe = () => (
+    <Container>
+        <div className="mx-auto w-md flex flex-col items-center h-screen">
+            Please, dont go away!! 
+        </div>
+    </Container>
+)
+export default Unsubscribe;
