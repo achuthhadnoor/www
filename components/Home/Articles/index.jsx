949c415462de2ca42d58ad0844d35bf8b68e60f5
Achuth Hadnoor <armorachyuth123@gmail.com>
2021-05-02 16:11:33 +0530

    blog view

--- components/Home/Articles/index.jsx
+++ components/Home/Articles/index.jsx
@@ -23,8 +23,8 @@ const Articles = ({ list, view }) => {
                                                 <div className={`mt-5 ${view === 'grid' && `max-w-md`}`}>
                                                     <h3 className="font-semibold  hover:bg-yellow-100 inline-block">{article.title}</h3>
                                                     <div className="my-2 text-xs flex flex-col sm:flex-row">
-                                                        <span className="text-gray-500 p-1">{format(parseISO(article.publishedAt), 'MMMM dd, yyyy')}</span>
-                                                        <div class="flex space-x-2 flex-1 flex-wrap space-y-2 items-center">
+                                                        <span className="text-gray-500 py-1">{format(parseISO(article.publishedAt), 'MMMM dd, yyyy')}</span>
+                                                        <div class="flex space-x-2 flex-1 flex-wrap items-center">
                                                             {
                                                                 article.tags.map((tag, index) => (
                                                                     <span className={`uppercase p-1 mx-2 rounded-md bg-gray-200 text-gray-600 text-xs inline-block`} key={`tag-${index}`}> {tag.value}</span>
@@ -40,16 +40,18 @@ const Articles = ({ list, view }) => {
                                 </>}
                             { view === 'list' &&
                                 (<Link href={`/blog/${article.slug}`}>
-                                    <a>
-                                        <div className={`mt-5 max-w-2xl border-b-2 pb-4 sm:pr-2`}>
+                                    <a className="border-b-2 pb-4 sm:border-b-0 sm:pr-2">
+                                        <div className={`mt-5 ${view === 'grid' && `max-w-md`}`}>
                                             <h3 className="font-semibold  hover:bg-yellow-100 inline-block">{article.title}</h3>
-                                            <div className="my-2 text-xs">
-                                                <span className="text-gray-500">{format(parseISO(article.publishedAt), 'MMMM dd, yyyy')}</span>
-                                                {
-                                                    article.tags.map((tag, index) => (
-                                                        <span className={`uppercase p-1 mx-2 rounded-md bg-gray-200 text-gray-600 text-xs`} key={`tag-${index}`}> {tag.value}</span>
-                                                    )
-                                                    )}
+                                            <div className="my-2 text-xs flex flex-col sm:flex-row">
+                                                <span className="text-gray-500 py-1">{format(parseISO(article.publishedAt), 'MMMM dd, yyyy')}</span>
+                                                <div class="flex space-x-2 flex-1 flex-wrap items-center">
+                                                    {
+                                                        article.tags.map((tag, index) => (
+                                                            <span className={`uppercase p-1 mx-2 rounded-md bg-gray-200 text-gray-600 text-xs inline-block`} key={`tag-${index}`}> {tag.value}</span>
+                                                        )
+                                                        )}
+                                                </div>
                                             </div>
                                             <p className={`text-gray-600 ${view === 'grid' && `text-sm`}`}>{article.summary}</p>
                                         </div>
