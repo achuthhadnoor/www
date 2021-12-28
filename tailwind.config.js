a6131f251dec0e0971031040b7658bbefad1178f
achuthhadnoor <armorachyuth123@gmail.com>
2021-12-28 09:36:53 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  jit:true,
+  content: ['./pages/**/*.tsx', './components/**/*.tsx', './layouts/**/*.tsx'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-opaque': 'rgb(13 42 148 / 18%)',
+        gray: {
+          0: '#fff',
+          100: '#fafafa',
+          200: '#eaeaea',
+          300: '#999999',
+          400: '#888888',
+          500: '#666666',
+          600: '#444444',
+          700: '#333333',
+          800: '#222222',
+          900: '#111111'
+        }
+      },
+    }
   },
-  plugins: [],
+  variants: {
+    typography: ['dark']
+  },
+  plugins: []
 }
