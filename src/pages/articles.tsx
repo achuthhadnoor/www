f1897d70995756b81c5fbc44aeec1478045ba48c
achuthhadnoor <armorachyuth123@gmail.com>
2022-01-03 20:42:20 +0530

    fix: article page responsive

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -23,16 +23,16 @@ export default function Blog({
         <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
           Articles
         </h1>
-        <p className="mb-4 text-gray-600 dark:text-gray-400 max-w-lg">
+        <p className="mb-4 text-gray-600 dark:text-gray-400 max-w-2xl">
           {`I write mostly about web development and tech careers. In total, I've written ${posts.length} articles on this site. Use the search below to filter by title.`}
         </p>
-        <div className="relative w-full mb-4">
+        <div className="relative w-full max-w-2xl mb-4">
           <input
             aria-label="Search articles"
             type="text"
             onChange={(e) => setSearchValue(e.target.value)}
             placeholder="Search articles"
-            className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-200 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:text-gray-100"
+            className="block w-full px-4 py-2 text-gray-900 bg-white border border-gray-200 rounded-md dark:border-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-yellow-800 dark:focus:border-yellow-800 dark:bg-gray-800 dark:text-gray-100"
           />
           <svg
             className="absolute w-5 h-5 text-gray-400 right-3 top-3 dark:text-gray-300"
