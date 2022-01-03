df60af5fea358e49ba2ea14c5b20c7f27c4ebbbb
achuthhadnoor <armorachyuth123@gmail.com>
2022-01-03 20:03:22 +0530

    homepage articles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,33 +18,35 @@ const RecentArticles = ({ posts }: { posts: any }) => {
       {posts.map(
         ({ slug, title, summary, publishedAt, tags }: any, i: number) => (
           <div
-            className=" sm:odd:mr-2 border-gray-50 dark:border-gray-800 border-2 text-sm border-b-2 py-4 sm:border-b-0 px-2 rounded"
+            className=" my-2 sm:odd:mr-2py-4 sm:border-b-0 px-2 rounded"
             key={`link-${i}`}
           >
-            <Link href={`/blog/${slug}`}>
-              <a className="flex  ">
-                <h4 className="font-semibold  dark:text-gray-100 hover:dark:text-yellow-500 hover:text-yellow-600 transition">
-                  {title}
-                </h4>
-              </a>
-            </Link>
-            <div className="my-2 text-xs flex flex-col">
-              <div className="flex space-x-2 flex-1 flex-wrap items-center text-sm pb-2">
-                {tags &&
-                  JSON.parse(tags).map((tag: string, i: number) => (
-                    <span
-                      key={`tag-key-${i}`}
-                      className="cursor-pointer select-none p-1 mr-2 rounded-md bg-gray-200 dark:bg-yellow-800 text-gray-600 dark:text-gray-200 text-xs inline-block"
-                    >
-                      #{tag}
-                    </span>
-                  ))}
+            <div className=" border-gray-50 dark:border-gray-800 border-2 text-sm border-b-2 p-2 rounded hover:shadow-md transition">
+              <Link href={`/blog/${slug}`}>
+                <a className="flex  ">
+                  <h4 className="font-semibold  dark:text-gray-100 hover:dark:text-yellow-500 hover:text-yellow-600 transition ">
+                    {title}
+                  </h4>
+                </a>
+              </Link>
+              <div className="my-2 text-xs flex flex-col">
+                <div className="flex space-x-2 flex-1 flex-wrap items-center text-sm pb-2">
+                  {tags &&
+                    JSON.parse(tags).map((tag: string, i: number) => (
+                      <span
+                        key={`tag-key-${i}`}
+                        className="cursor-pointer select-none p-1 mr-2 rounded-md bg-gray-200 dark:bg-yellow-800 text-gray-600 dark:text-gray-200 text-xs inline-block"
+                      >
+                        #{tag}
+                      </span>
+                    ))}
+                </div>
+                <span className="text-gray-500 py-1 dark:text-gray-300">
+                  {format(parseISO(publishedAt), "MMMM dd, yyyy")}
+                </span>
               </div>
-              <span className="text-gray-500 py-1 dark:text-gray-300">
-                {format(parseISO(publishedAt), "MMMM dd, yyyy")}
-              </span>
+              <p className="text-gray-300 text-md">{summary}</p>
             </div>
-            <p className="text-gray-300 text-md">{summary}</p>
           </div>
         )
       )}
