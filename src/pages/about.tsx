aa6a5248a332117f57dca061fa5d3efe607362d3
achuthhadnoor <armorachyuth123@gmail.com>
2022-09-12 04:16:08 +0530

    projects page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,11 +9,18 @@ export default function Home() {
         height={650}
         width={1080}
         src={"/assets/img/achuth.jpg"}
+        alt="Achuth Hadnoor"
       />
       <div className="my-5">
-        <h1 className="font-semibold text-3xl md:text-5xl">About me</h1>
+        <h1 className="font-semibold text-3xl md:text-5xl my-5">About me</h1>
         <p className="mt-2 text-sm leading-loose text-neutral-700 dark:text-neutral-500">
-          I am a web developer
+          Innovative, task-driven professional with 6+ years of experience in
+          web design and development across diverse industries. Equipped with a
+          record of success in consistently identifying and providing the
+          technological needs of companies through ingenious innovation.
+          Proficient in developing applications, creating user interfaces,
+          writing and testing codes, troubleshooting simple/complex issues, and
+          implementing new features based on user feedback.
         </p>
       </div>
       <hr className="wave my-20" />
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,13 +1,74 @@
+import Image from "next/image";
 import Container from "../layouts/Container";
 
 export default function Home() {
+  const projects = [
+    {
+      id: "lapse",
+      title: "Lapse",
+      description: "A simple app to record screen in time-lapse.",
+      url: "getlapseapp.com",
+      image: "/assets/img/lapse.png",
+    },
+    {
+      id: "focus",
+      title: "Focus",
+      description: "Take timeoff from screen to focus",
+      url: "focus.achuth.dev",
+      image: "/assets/img/focus.png",
+    },
+    {
+      id: "snipcode",
+      title: "snipcode",
+      description: "Code snippet manager on menubar",
+      url: "snipper.netlify.app",
+      image: "/assets/img/snipcode.png",
+    },
+    {
+      id: "snip",
+      title: "snip",
+      description: "Single line code snippet manager on menubar",
+      url: "snip.netlify.app",
+      image: "/assets/img/snip.png",
+    },
+    {
+      id: "snipnote",
+      title: "snipnote",
+      description: "Simple note taking on menubar",
+      url: "snipnote.achuth.dev",
+      image: "/assets/img/snipnote.png",
+    },
+  ];
   return (
     <Container>
       <h1 className="font-semibold text-3xl md:text-5xl mb-10">Projects</h1>
       <p className="mt-2 text-md leading-loose text-neutral-700 dark:text-neutral-500">
         Here’s a small selection of some of my recent projects and experiences.
       </p>
-      <hr className="wave my-20" />
+      <hr className="wave my-10" />
+      {projects.map((project) => (
+        <div key={project.id}>
+          <Image
+            className="rounded-2xl "
+            height={750}
+            width={1080}
+            src={project.image}
+            alt="Lapse app"
+          />
+          <h2 className="font-semibold text-xl md:text-3xl my-10">
+            {project.title}
+          </h2>
+          <p className="my-5">{project.description}</p>
+          <a
+            className="rounded-full py-1 px-4 bg-neutral-800 hover:bg-blue-600"
+            href={`https://${project.url}`}
+          >
+            <span>{project.url}</span>
+            <span className="transform scale-110"> ↗ </span>
+          </a>
+          <hr className="wave my-10" />
+        </div>
+      ))}
     </Container>
   );
 }
