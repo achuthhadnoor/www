4ca75f59bcd84bea7d9909ed6b98530dbff9c18c
Achuth <armorachyuth123@gmail.com>
2023-08-08 12:04:36 +0000

    [header] change in position and Ui

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,36 +1,64 @@
 "use client";
-import Link from "next/link";
+import NextLink from "next/link";
 import React, { useState } from "react";
 import Icon from "react-icons-kit";
-import { download, sun, moon, airplay, menu } from "react-icons-kit/feather";
+import { download, sun, moon, airplay, menu, x, home } from "react-icons-kit/feather";
+import cl from 'classnames'
+import { useRouter } from "next/router";
+
+const Link = ({ href, ...props }: any) => {
+  const router = useRouter();
+  const isActive =
+    href === "/" ? router.asPath === href : router.asPath.includes(href);
+  return (
+    <NextLink href={href}>
+      <a
+        className={cl(
+          isActive
+            ? // ? "bg-neutral-300  dark:bg-neutral-800"
+            // : " hover:bg-neutral-300 dark:hover:bg-neutral-800",
+            "text-indigo-400"
+            : " hover:text-indigo-500 dark:hover:text-indigo-500",
+          "relative rounded-md p-2"
+        )}
+        {...props}
+      >
+        {name}
+        {isActive && (
+          <span className="absolute inset-x-1 -bottom-3 h-px select-none bg-gradient-to-r from-indigo-500/0 via-indigo-500/40 to-indigo-500/0 focus:outline-none dark:from-indigo-400/0 dark:via-indigo-400/40 dark:to-indigo-400/0"></span>
+        )}
+      </a>
+    </NextLink>
+  );
+};
 
 export default function Header() {
-  const [open, setOpen] = useState(false);
-  const navigation = [
-    {
-      link: "/",
+  const [isOpen, setIsOpen] = useState(true);
+  const navigation = {
+    "/": {
       text: "Home",
+      icon: '🏠',
       external: false,
     },
-    {
-      link: "/craft",
+    "/craft": {
       text: "Craft",
+      icon: '🔬',
       external: false,
     },
-    {
-      link: "/thoughts",
+    "/thoughts": {
       text: "Thoughts",
+      icon: '🗒️',
       external: false,
     },
-    {
-      link: "/about",
+    "/about": {
       text: "About",
+      icon: '👨‍💻',
       external: false,
     },
-  ];
+  };
   return (
     <header className="fixed bottom-2 w-full">
-      <div className="z-20 bg-neutral-800/60 backdrop-blur-md  m-2  rounded-md ">
+      <div className="z-20 bg-neutral-800/60 backdrop-blur-md  m-2 border border-b-0 border-neutral-300/10 rounded-md ">
         <nav className="items-center bottom-5 flex justify-between">
           <span className="p-2">
             <svg
@@ -69,19 +97,21 @@ export default function Header() {
           <span className=" cursor-pointer p-2">
             <Icon size={18} icon={airplay} />
           </span> */}
-            <span className=" cursor-pointer p-2">
-              <Icon size={18} icon={menu} />
-            </span>
+            {isOpen ? <span className=" cursor-pointer p-2">
+              <Icon size={18} icon={x} />
+            </span> : <span className="cursor-pointer p-2">
+              <Icon icon={menu} size={18} />
+            </span>}
           </div>
         </nav>
         <div className="h-[1px] rounded-full mx-2 bg-gradient-to-r from-indigo-900/50 via-yellow-800/50 to-red-900/50" />
       </div>
-      <nav className="z-10 fixed bottom-16 p-2 w-full text-sm">
-        <ul className="flex flex-col bg-neutral-800/60 backdrop-blur-md p-2 rounded-md gap-4 text-right px-6">
-          {navigation.map((nav) => (
-            <Link key={`link-${nav.text}`} href={nav.link}>
-              <li className="p-2">{nav.text}</li>
-            </Link>
+      <nav className={cl(isOpen ? "bottom-14" : "-bottom-[100%]", "z-10 fixed p-2 w-full text-sm max-w-md right-0 ")}>
+        <ul className="flex flex-col bg-neutral-800/60 backdrop-blur-md py-5 rounded-md gap-4 px-2 border border-b-0 border-neutral-300/10">
+          {Object.entries(navigation).map(([path, link]) => (
+            <NextLink key={`link-${link.text}`} href={path} >
+              <li className="p-2 rounded-md px-2">{link.icon} {link.text}</li>
+            </NextLink>
           ))}
         </ul>
       </nav>
